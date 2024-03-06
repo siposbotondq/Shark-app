e9b9cdd4108698bcd916425c3e49d40761f3283b
siposbotondq <162465074+siposbotondq@users.noreply.github.com>
2024-03-06 15:33:26 +0100

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Shark
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private MainPageTimer mainPageTimer;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            mainPageTimer = new MainPageTimer();
+            BindingContext = mainPageTimer;
+        }
+        private void Button_ClickedStart(object sender, EventArgs e)
+        {
+            mainPageTimer.StartTimer();
+        }
+        private void Button_ClickedStop(object sender, EventArgs e)
+        {
+            mainPageTimer.StopTimer();
+        }
+        private void Button_ClickedReset(object sender, EventArgs e)
+        {
+            mainPageTimer.ResetTimer();
+        }
+
+
+    }
+    
+}
---
+++ MainPageTimer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using System.Timers;
+using Xamarin.Forms;
+
+namespace Shark
+{
+    public class MainPageTimer : INotifyPropertyChanged
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        private Timer time = new Timer();
+        private bool timerRunning;
+
+        private string _stopWatchHours;
+        public string StopWatchHours
+        {
+            get { return _stopWatchHours; }
+            set
+            {
+                _stopWatchHours = value;
+                OnPropertyChanged("StopWatchHours");
+            }
+        }
+        private string _stopWatchMinutes;
+        public string StopWatchMinutes
+        {
+            get { return _stopWatchMinutes; }
+            set
+            {
+                _stopWatchMinutes = value;
+                OnPropertyChanged("StopWatchMinutes");
+            }
+        }
+        private string _stopWatchSeconds;
+        public string StopWatchSeconds
+        {
+            get { return _stopWatchSeconds; }
+            set
+            {
+                _stopWatchSeconds = value;
+                OnPropertyChanged("StopWatchSeconds");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var changed = PropertyChanged;
+            if (changed != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public MainPageTimer()
+        {
+            stopWatch.Start();
+            StopWatchHours = stopWatch.Elapsed.Hours.ToString();
+            StopWatchMinutes = stopWatch.Elapsed.Minutes.ToString();
+            StopWatchSeconds = stopWatch.Elapsed.Seconds.ToString();
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                StopWatchHours = stopWatch.Elapsed.Hours.ToString();
+                StopWatchMinutes = stopWatch.Elapsed.Minutes.ToString();
+                StopWatchSeconds = stopWatch.Elapsed.Seconds.ToString();
+                return true;
+            });
+
+        }
+        public void ResetTimer()
+        {
+            stopWatch.Reset();
+        }
+        public void StopTimer()
+        {
+            stopWatch.Stop();
+        }
+        
+        public void StartTimer()
+        {
+            stopWatch.Start();
+        }
+
+    }
+    
+}
